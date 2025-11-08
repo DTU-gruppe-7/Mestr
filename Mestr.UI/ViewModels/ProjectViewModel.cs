974b81b74173ee75e6ff0a6fc9f5fc0aa61007e9
PhilipMJ <s245865@student.dtu.dk>
2025-11-08 12:03:35 +0100

    Add BaseViewModel and ProjectViewModel with data bindings

---
+++ Mestr.UI/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Mestr.UI.ViewModels
+{
+    public class ProjectViewModel : ViewModelBase
+    {
+        private string _projectName;
+        public string ProjectName
+        {
+            get { return _projectName; }
+
+            set
+            {
+                _projectName = value;
+                OnPropertyChanged(nameof(ProjectName));
+            }
+        }
+
+
+        private string _clientName;
+        public string ClientName
+        {
+            get { return _clientName; }
+            set
+            {
+                _clientName = value;
+                OnPropertyChanged(nameof(ClientName));
+            }
+        }
+
+
+        private DateTime _deadline;
+        public DateTime Deadline
+        {
+            get { return _deadline; }
+            set
+            {
+                _deadline = value;
+                OnPropertyChanged(nameof(Deadline));
+            }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+                OnPropertyChanged(nameof(Description));
+            }
+
+        }
+
+        public ICommand CreateProjectCommand { get; }
+        public ICommand CancelCommand { get; }
+    }
+
+}
