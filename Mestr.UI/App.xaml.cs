b265fe8d09fa41a871c78fd76de4efe7ff4e1158
Mathias Rasmussen <s235447@student.dtu.dk>
2025-11-22 17:24:51 +0100

    Fixed wrong format on startup

--- Mestr.UI/App.xaml.cs
+++ Mestr.UI/App.xaml.cs
@@ -15,19 +15,9 @@ namespace Mestr.UI
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            QuestPDF.Settings.License = LicenseType.Community;
-            MainWindow = new MainWindow()
-            {
-                DataContext = new MainViewModel(),
-            };
-
-            MainWindow.Show();
-
-            base.OnStartup(e);
-
+            // Sæt kultur FØRST, før noget UI oprettes
             var cultureInfo = new CultureInfo("da-DK");
 
-            // Sæt det som standard for hele tråden (logik) og UI (visning)
             Thread.CurrentThread.CurrentCulture = cultureInfo;
             Thread.CurrentThread.CurrentUICulture = cultureInfo;
 
@@ -37,7 +27,15 @@ namespace Mestr.UI
                 new FrameworkPropertyMetadata(
                     System.Windows.Markup.XmlLanguage.GetLanguage(cultureInfo.IetfLanguageTag)));
 
+            base.OnStartup(e);
+
+            QuestPDF.Settings.License = LicenseType.Community;
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel(),
+            };
+
+            MainWindow.Show();
         }
     }
-
 }
